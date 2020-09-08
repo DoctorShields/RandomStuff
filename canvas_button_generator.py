f4d7639a19a233eb263367584fe14cada8b3aa5d
Dr. Shields <31747379+DoctorShields@users.noreply.github.com>
2020-09-07 21:05:38 -0400

    Update canvas_button_generator.py

--- canvas_button_generator.py
+++ canvas_button_generator.py
@@ -8,7 +8,8 @@
 course = "31082"
 
 str1 = "<a style=\"background-color: #f97a07; border: 1px solid #0b0e07; font-size: 35px; padding: 9px 23px; text-align: center; margin: 30px 50px; display:block; text-decoration: none; color: #ffffff;\" href="
-str2 = " data-api-endpoint=\"https://cvilleschools.instructure.com/api/v1/courses/"
+str2 = " target=\"_"
+str21 = "\" data-api-endpoint=\"https://cvilleschools.instructure.com/api/v1/courses/"
 str25 = "/modules\" data-api-returntype=\"[Module]\">"
 str26 = " <img src=\"https://cvilleschools.instructure.com/courses/"
 str3 = "/files/"
@@ -21,22 +22,27 @@ str5 = "\" data-api-returntype=\"File\" /></a>"
 #         "https://charlottesvilleschools.zoom.us/my/drshields"]
 #phys
 #links = ["https://cvilleschools.instructure.com/courses/" + course + "/modules",
-#         "https://charlottesvilleschools.zoom.us/meeting/81459666176",
+#         "https://charlottesvilleschools.zoom.us/j/81459666176",
 #         "https://charlottesvilleschools.zoom.us/my/drshields"]
 #capstone
 links = ["https://cvilleschools.instructure.com/courses/" + course + "/modules",
-         "https://charlottesvilleschools.zoom.us/meeting/88921166863",
+         "https://charlottesvilleschools.zoom.us/j/88921166863",
          "https://charlottesvilleschools.zoom.us/my/drshields"]
 #eng4
 #links = ["https://cvilleschools.instructure.com/courses/" + course + "/modules",
-#         "https://charlottesvilleschools.zoom.us/meeting/83482605350",
-#         "https://charlottesvilleschools.zoom.us/meeting/86184753524",
+#         "https://charlottesvilleschools.zoom.us/j/83482605350",
+#         "https://charlottesvilleschools.zoom.us/j/86184753524",
 #         "https://charlottesvilleschools.zoom.us/my/drshields"]
 
 #eng 3, phys, capstone
 txt = ["Assignments", "Class Meetings", "Office Hours"]
 #eng 4
 #txt = ["Assignments", "1st Period Class Meetings", "4th Period Class Meetings", "Office Hours"]
+
+#eng 3, phys, capstone
+target = ["self", "blank", "blank"]
+#eng 4
+#txt = ["self", "blank", "blank", "blank"]
 
 #eng3
 #imgs = ["1659567", "1659566", "1659566"]
@@ -48,5 +54,5 @@ txt = ["Assignments", "Class Meetings", "Office Hours"]
 imgs = ["1663101", "1663102", "1663102"]
 
 for i, link in enumerate(links):
-    out = str1 + link + str2 + course + str25 + txt[i] + str26 + course + str3 + imgs[i] + str4 + course + str5
+    out = str1 + link + str2 + target[i] + str21 + course + str25 + txt[i] + str26 + course + str3 + imgs[i] + str4 + course + str5
     print(out)
